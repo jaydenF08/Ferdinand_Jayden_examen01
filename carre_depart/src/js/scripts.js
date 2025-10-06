3dbbc27474cecfca6c1340de9b16f4c05a948fc7
jaydenF08 <142918504+jaydenF08@users.noreply.github.com>
2025-10-06 13:25:24 -0400

    fin

--- carre_depart/src/js/scripts.js
+++ carre_depart/src/js/scripts.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
     document.querySelector(".col").addEventListener("change", function () {
       const htmlElement = document.documentElement;
-      const currentCarte = htmlElement.getAttribute("carte");
+      const currentCarte = htmlElement.getAttribute("col");
   
       if (currentCarte === "d-none") {
         htmlElement.setAttribute("carte", "none");
       } else {
-        htmlElement.setAttribute("carte", "d-none");
+        htmlElement.setAttribute("back", "d-none");
       }
     });
   });
